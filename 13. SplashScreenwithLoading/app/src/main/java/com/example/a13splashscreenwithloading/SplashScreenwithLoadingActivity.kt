98b29226121f2bba4279a9242517dea89fa8e945
Vishal <chitrodevishal@gmail.com>
2024-04-11 22:03:53 +0530

    commit

---
+++ 13. SplashScreenwithLoading/app/src/main/java/com/example/a13splashscreenwithloading/SplashScreenwithLoadingActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a13splashscreenwithloading
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenwithLoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screenwith_loading)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
