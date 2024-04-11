2833310e3e93dfc9046b992101c0694c5a98724c
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-11 13:54:29 +0530

    Commit

---
+++ 4.ImageViewandEdittext/app/src/main/java/com/example/a4imageviewandedittext/ImageViewActivity.kt
@@ -0,0 +1,29 @@
+package com.example.a4imageviewandedittext
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a4imageviewandedittext.databinding.ActivityImageViewBinding
+
+class ImageViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_image_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+            val button:Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this,"Signup Successfully",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
---
+++ 7. ConstraintLayout/app/src/main/java/com/example/a7constraintlayout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.a7constraintlayout
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+    }
+}
