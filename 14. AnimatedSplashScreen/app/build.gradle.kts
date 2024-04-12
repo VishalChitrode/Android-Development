905e18f102caac1e717cedcc15bc4f5f6365c1e6
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-12 12:42:57 +0530

    commit

--- 14. AnimatedSplashScreen/app/build.gradle.kts
+++ 14. AnimatedSplashScreen/app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("com.airbnb.android:lottie:6.4.0")
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
 }
---
+++ 14. AnimatedSplashScreen/app/src/main/java/com/example/a14animatedsplashscreen/AnimatedSplashScreenWithGIFActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a14animatedsplashscreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AnimatedSplashScreenWithGIFActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_animated_splash_screen_with_gifactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
