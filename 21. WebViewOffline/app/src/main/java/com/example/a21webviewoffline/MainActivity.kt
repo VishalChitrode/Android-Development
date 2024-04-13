4807256fbccb9f146b0dc20811baa061ecb36216
Vishal <chitrodevishal@gmail.com>
2024-04-13 16:41:03 +0530

    commit

--- 21. WebViewOffline/app/src/main/java/com/example/a21webviewoffline/MainActivity.kt
+++ 21. WebViewOffline/app/src/main/java/com/example/a21webviewoffline/MainActivity.kt
@@ -1,20 +1,17 @@
-package com.example.a20webviews
+package com.example.a21webviewoffline
 
 import android.os.Bundle
-import android.view.KeyEvent
 import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.a20webviews.databinding.ActivityMainBinding
+import com.example.a21webviewoffline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private  val binding : ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,27 +21,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        supportActionBar?.show()
-
-        binding.webView.loadUrl("https://www.amazon.in/")
+        binding.webView.loadUrl("file:///android_asset/index.html")
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.webViewClient = WebViewClient()
-
-
-
-
     }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        // Check whether the key event is the Back button and if there's history.
-        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webView.canGoBack()) {
-            binding.webView.goBack()
-            return true
-        }
-        // If it isn't the Back button or there isn't web page history, bubble up to
-        // the default system behavior. Probably exit the activity.
-        return super.onKeyDown(keyCode, event)
-    }
-
-
 }
