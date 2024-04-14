4bdc29c578cbbffd4a2cdb5a5fd769c0a4e21cf2
Vishal <chitrodevishal@gmail.com>
2024-04-14 22:16:08 +0530

    commit

--- 24. Fragments/app/src/main/java/com/example/a24fragments/MainActivity.kt
+++ 24. Fragments/app/src/main/java/com/example/a24fragments/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a24fragments.databinding.ActivityMainBinding
+import com.example.a24fragments.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
+
+   val binding : ActivityMainBinding by lazy {
+       ActivityMainBinding.inflate(layoutInflater)
+   }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val manager = supportFragmentManager
+        val tm = manager.beginTransaction()
     }
 }
