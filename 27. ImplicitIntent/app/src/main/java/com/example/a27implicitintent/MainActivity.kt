6e23ddea7d3d5777768e1c072418bb478f9d420f
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-16 16:55:58 +0530

    Commit

--- 27. ImplicitIntent/app/src/main/java/com/example/a27implicitintent/MainActivity.kt
+++ 27. ImplicitIntent/app/src/main/java/com/example/a27implicitintent/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.a27implicitintent
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a27implicitintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.webpage.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.geeksforgeeks.org/implicit-and-explicit-intents-in-android-with-examples/")
+            )
+            startActivity(intent)
+        }
+        binding.opencamera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+        binding.makecall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:+917987265650")
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            val text = binding.editTextText.toString()
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, text)
+            startActivity(Intent.createChooser(intent, "Share Via"))
         }
     }
 }
