3b4438dcf137b3dbafb655b4687c703a3dd32d1c
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-18 11:35:45 +0530

    commit

--- 27. ImplicitIntent/app/src/main/java/com/example/a27implicitintent/MainActivity.kt
+++ 27. ImplicitIntent/app/src/main/java/com/example/a27implicitintent/MainActivity.kt
@@ -12,14 +12,14 @@ import com.example.a27implicitintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private  val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private  val binding: ActivityMainBinding by lazy { // here we enable view binding
+        ActivityMainBinding.inflate(layoutInflater) //
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(binding.root) // with the help of this we can access all views without find by id's
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -27,27 +27,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.webpage.setOnClickListener {
-            val intent = Intent(
+            val intent = Intent( // here we make intent variables and we call intents method
                 Intent.ACTION_VIEW,
                 Uri.parse("https://www.geeksforgeeks.org/implicit-and-explicit-intents-in-android-with-examples/")
-            )
-            startActivity(intent)
+            ) // for URL we use Uri parse method
+            startActivity(intent) // Here we start intent
         }
         binding.opencamera.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) // here we create a intent variable and use Mediastore with Action Image Capture
             startActivity(intent)
         }
         binding.makecall.setOnClickListener {
-            val intent = Intent(Intent.ACTION_DIAL)
-            intent.data = Uri.parse("tel:+917987265650")
-            startActivity(intent)
+            val intent = Intent(Intent.ACTION_DIAL) // Action Dial here
+            intent.data = Uri.parse("tel:+917987265650") // here we insert data for calling
+            startActivity(intent) // here we start intent
         }
         binding.button.setOnClickListener {
-            val text = binding.editTextText.toString()
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT, text)
-            startActivity(Intent.createChooser(intent, "Share Via"))
+            val text = binding.editTextText.text.toString() // here we create variable and get text with help of toString()
+            val intent = Intent(Intent.ACTION_SEND) // here we create intent var and we use to share the above text
+            intent.type = "text/plain" // here we define intent type
+            intent.putExtra(Intent.EXTRA_TEXT, text) // here we
+            startActivity(Intent.createChooser(intent, "Share Via")) // here we use creatorchooser
         }
     }
 }
