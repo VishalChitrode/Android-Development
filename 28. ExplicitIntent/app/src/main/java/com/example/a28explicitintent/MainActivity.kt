c14dfcc0d1d7b868030d3ca653ab39d4e69c7a96
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-18 11:53:55 +0530

    commit

--- 28. ExplicitIntent/app/src/main/java/com/example/a28explicitintent/MainActivity.kt
+++ 28. ExplicitIntent/app/src/main/java/com/example/a28explicitintent/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.a28explicitintent
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a28explicitintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater) //here we use binding
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root) // here we use binding.root
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.button.setOnClickListener {
+ // here we use binding to find button and use setonclicklistner
+            val intent  = Intent(this,ProfileActivity::class.java) // for main activity we use this keyword and gave other activity id's and ::class.java
+            startActivity(intent) // here we start activity and gave intents
+
         }
     }
 }
