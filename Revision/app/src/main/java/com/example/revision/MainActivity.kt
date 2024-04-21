99e43f1a49e4a98e5b39e87d7fb0f341c6c831c5
Vishal <chitrodevishal@gmail.com>
2024-04-21 22:15:38 +0530

    commit

--- Revision/app/src/main/java/com/example/revision/MainActivity.kt
+++ Revision/app/src/main/java/com/example/revision/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.revision
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Display.Mode
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +10,9 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.revision.databinding.ActivityMainBinding
+import com.example.revision.databinding.ChatcardviewBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -29,26 +33,51 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val bottomBar = findViewById<AnimatedBottomBar>(R.id.bottom_bar)
+
+        bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(lastIndex: Int, lastTab: AnimatedBottomBar.Tab?, newIndex: Int, newTab: AnimatedBottomBar.Tab) {
+                when (newTab.id) {
+                    R.id.updates -> {
+                        // Handle updates tab click
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main, UpdatesFragment())
+                            .commit()
+                        true
+                    }
+                    R.id.communities -> {
+                        // Handle communities tab click
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main, CommunitiesFragment())
+                            .commit()
+                        true
+                    }
+                    R.id.calls -> {
+                        // Handle calls tab click
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main, CallsFragment())
+                            .commit()
+                        true
+                    }
+                }
+            }
+
+            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                // Handle tab reselection
+            }
+        })
+
         datalist = ArrayList<ItemsViewModel>()
         datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","12:00 pm",R.drawable.pin))
         datalist.add(ItemsViewModel(R.drawable.krishan,"Pritesh chitrode","Hey","11:00 pm",R.drawable.pin))
         datalist.add(ItemsViewModel(R.drawable.krishan,"Pooja chitrode","Hii","10:00 pm",R.drawable.pin))
         datalist.add(ItemsViewModel(R.drawable.krishan,"Deepak chitrode","Busy","9:00 pm",R.drawable.pin))
         datalist.add(ItemsViewModel(R.drawable.krishan,"Pushpa chitrode","WhatsApp Clone","8:00 pm",R.drawable.pin))
-//        datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","7:00 pm",R.drawable.pin))
-//        datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","6:00 pm",R.drawable.pin))
-//        datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","1:00 pm",R.drawable.pin))
-//        datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","1:00 pm",R.drawable.pin))
-//        datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","1:00 pm",R.drawable.pin))
-//        datalist.add(ItemsViewModel(R.drawable.krishan,"Vishal chitrode","Hello","1:00 pm",R.drawable.pin))
 
         adapter = CustomAdapter(datalist,this)
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
         binding.recyclerview.adapter = adapter
-
-
     }
-
-
-
 }
--- Revision/app/src/main/java/com/example/revision/UpdatesFragment.kt
+++ Revision/app/src/main/java/com/example/revision/UpdatesFragment.kt
@@ -5,29 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.revision.databinding.ActivityMainBinding
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UpdatesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UpdatesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class UpdatesFragment : Fragment() {
+
+    private lateinit var bottomBar: AnimatedBottomBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +26,47 @@ class UpdatesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_updates, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UpdatesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UpdatesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bottomBar = view.findViewById(R.id.bottom_bar)
+
+        bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+            override fun onTabSelected(lastIndex: Int, lastTab: AnimatedBottomBar.Tab?, newIndex: Int, newTab: AnimatedBottomBar.Tab) {
+                when (newTab.id) {
+                    R.id.updates -> {
+                        // Handle updates tab click
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.updates, UpdatesFragment())
+                            .commit()
+                        true
+                    }
+                    R.id.communities -> {
+                        // Handle communities tab click
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.updates, CommunitiesFragment())
+                            .commit()
+                        true
+                    }
+                    R.id.calls -> {
+                        // Handle calls tab click
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.updates, CallsFragment())
+                            .commit()
+                        true
+                    }
                 }
             }
+
+            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                // Handle tab reselection
+            }
+        })
+    }
+
+    companion object {
+        fun newInstance(): UpdatesFragment {
+            return UpdatesFragment()
+        }
     }
 }
