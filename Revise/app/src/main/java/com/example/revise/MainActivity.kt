1d6a3ceb5c0ad30b3b11944385c37cd3a5cca32d
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-23 16:22:14 +0530

    commit

--- Revise/app/src/main/java/com/example/revise/MainActivity.kt
+++ Revise/app/src/main/java/com/example/revise/MainActivity.kt
@@ -1,61 +1,79 @@
 package com.example.revise
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
-import com.example.revise.fragment.CallsFragment
-import com.example.revise.fragment.ChatsFragment
-import com.example.revise.fragment.CommunitiesFragment
-import com.example.revise.fragment.StatusFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.revise.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNav : BottomNavigationView
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private lateinit var auth: FirebaseAuth
 
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.d(TAG,"Check User")
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.signincreatehere.setOnClickListener{
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
 
+        binding.signinbutton.setOnClickListener{
+            val signinusername = binding.signineditTextTextEmailAddress.text.toString()
+            val signinpassword = binding.siginpeditTextTextPassword.text.toString()
+            Log.d(TAG,"Variable initialize")
 
-        loadFragment(ChatsFragment())
-        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
-        bottomNav.selectedItemId = R.id.chat
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.chat -> {
-                    loadFragment(ChatsFragment())
-                    true
-                }
-                R.id.status -> {
-                    loadFragment(StatusFragment())
-                    true
-                }
-                R.id.communities -> {
-                    loadFragment(CommunitiesFragment())
-                    true
-                }
-                R.id.calls -> {
-                    loadFragment(CallsFragment())
-                    true
-                }
-                else -> {
-                    false
-                }
+            if (signinusername.isEmpty() || signinpassword.isEmpty()){
+                Toast.makeText(this,"Please filled the fields",Toast.LENGTH_SHORT).show()
+            }else {
+                auth.signInWithEmailAndPassword(signinusername,signinpassword)
+                    .addOnCompleteListener { taskId->
+                        if (taskId.isSuccessful){
+                            Toast.makeText(this,"Login Successfull",Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
             }
         }
-    }
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame,fragment)
-        transaction.commit()
-    }
-    }
+
+        binding.signincreatehere.setOnClickListener{
+            Toast.makeText(this,"Register yourself",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }}
