6086bb571da0597f5481403d923ebc8f3cb1ce88
Vishal chitrode <chitrodevishal@gmail.com>
2024-04-23 16:48:16 +0530

    commit

--- GoogleSignInFirebase/app/src/main/java/com/example/googlesigninfirebase/MainActivity.kt
+++ GoogleSignInFirebase/app/src/main/java/com/example/googlesigninfirebase/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.googlesigninfirebase
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.googlesigninfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+   private val binding: ActivityMainBinding by lazy {
+       ActivityMainBinding.inflate(layoutInflater)
+   }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.signinFacebooklogin.setOnClickListener{
+            Toast.makeText(this,"FaceBook",Toast.LENGTH_SHORT).show()
         }
     }
 }
